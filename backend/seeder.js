3abebd440e225b6dedb2fee36ed023575b6edf82
Roxanne Weber <89050736+roxanneweber@users.noreply.github.com>
2022-05-16 10:06:53 -0500

    import and destroy data seeder now successful

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,13 +1,12 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import colors from 'colors';
-import users from './data/users.js';
-import products from './data/products.js';
-
-import User from './models/userModel.js';
-import Product from './models/productModel.js';
-import Order from './models/orderModel.js';
-import connectDB from './config/db.js';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import colors from "colors";
+import users from "./data/users.js";
+import products from "./data/products.js";
+import User from "./models/userModel.js";
+import Product from "./models/productModel.js";
+import Order from "./models/orderModel.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
@@ -16,7 +15,7 @@ connectDB();
 const importData = async () => {
 	try {
 		await Order.deleteMany();
-		await Products.deleteMany();
+		await Product.deleteMany();
 		await User.deleteMany();
 
 		const createdUsers = await User.insertMany(users);
@@ -27,12 +26,12 @@ const importData = async () => {
 			return { ...product, user: adminUser };
 		});
 
-		await Product.insertMany(smapleProducts);
+		await Product.insertMany(sampleProducts);
 
-		console.log('Data Imported!'.green.inverse);
+		console.log("Data Imported!".green.inverse);
 		process.exit();
 	} catch (error) {
-		`${error}`.red.inverse;
+		console.error(`${error}`.red.inverse);
 		process.exit(1);
 	}
 };
@@ -40,18 +39,18 @@ const importData = async () => {
 const destroyData = async () => {
 	try {
 		await Order.deleteMany();
-		await Products.deleteMany();
+		await Product.deleteMany();
 		await User.deleteMany();
 
-		console.log('Data Destroyed!'.red.inverse);
+		console.log("Data Destroyed!".red.inverse);
 		process.exit();
 	} catch (error) {
-		`${error}`.red.inverse;
+		console.error(`${error}`.red.inverse);
 		process.exit(1);
 	}
 };
 
-if (process.argv[2] === '-d') {
+if (process.argv[2] === "-d") {
 	destroyData();
 } else {
 	importData();
