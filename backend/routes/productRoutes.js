a8631be60d6e236407a4c84d8f6ddefed6df9500
Roxanne Weber <89050736+roxanneweber@users.noreply.github.com>
2022-05-16 11:47:43 -0500

    error middleware added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,36 @@
 import express from "express";
+import asyncHandler from "express-async-handler";
 const router = express.Router();
+import Product from "../models/productModel.js";
 
-router.get("/", (req, res) => {
-	res.json(products);
-});
+// @desc    fetch all products
+// @route   GET /api/products
+// @access  public
+router.get(
+	"/",
+	asyncHandler(async (req, res) => {
+		const products = await Product.find({});
 
-router.get("/:id", (req, res) => {
-	const product = products.find((p) => p._id === req.params.id);
-	res.json(product);
-});
+		res.json(products);
+	})
+);
+
+// @desc    fetch single product
+// @route   GET /api/product/:id
+// @access  public
+router.get(
+	"/:id",
+	asyncHandler(async (req, res) => {
+		const product = await Product.findById(req.params.id);
+
+		if (product) {
+			res.json(product);
+		} else {
+			res.status(404).json({
+				message: "Product not found.",
+			});
+		}
+	})
+);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,30 @@ app.get("/", (req, res) => {
 	res.send("API is running...");
 });
 
+// routes
 app.use("/api/products", productRoutes);
 
+// error middleware
+
+app.use((req, res, next) => {
+	const error = new Error(`Not Found - ${req.originalUrl}`);
+	res.status(404);
+	next(error);
+});
+
+app.use((err, req, res, next) => {
+	const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+	res.status(statusCode);
+	res.json({
+		message: err.message,
+		stack: process.env.NODE_ENV === "production" ? null : err.stack,
+	});
+});
+
+// define PORT
 const PORT = process.env.PORT || 5000;
 
+// listen on port ...
 app.listen(
 	PORT,
 	console.log(
