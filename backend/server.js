925f9f1e3287cbc3ebdc11c97a67474d7f3b9afd
Roxanne Weber <89050736+roxanneweber@users.noreply.github.com>
2022-05-16 11:52:24 -0500

    added middleware folder to clean up server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import colors from "colors";
 
@@ -20,20 +21,8 @@ app.use("/api/products", productRoutes);
 
 // error middleware
 
-app.use((req, res, next) => {
-	const error = new Error(`Not Found - ${req.originalUrl}`);
-	res.status(404);
-	next(error);
-});
-
-app.use((err, req, res, next) => {
-	const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-	res.status(statusCode);
-	res.json({
-		message: err.message,
-		stack: process.env.NODE_ENV === "production" ? null : err.stack,
-	});
-});
+app.use(notFound);
+app.use(errorHandler);
 
 // define PORT
 const PORT = process.env.PORT || 5000;
